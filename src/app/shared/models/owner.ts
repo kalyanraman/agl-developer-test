20acf611e903b6cfd2807c0072d890bcd7fad08c
Kalyan Yegnavajjula <yv_raman@outlook.com>
2021-04-14 07:07:25 +1000

    Renamed properties of the Owner type

--- src/app/shared/models/owner.ts
+++ src/app/shared/models/owner.ts
@@ -2,8 +2,8 @@ import { Gender } from "./gender";
 import { Pet } from "./pet";
 
 export interface Owner {
-    Name: string;
-    Gender: Gender;
-    Age: Number;
-    Pets: Array<Pet>;
+    name: string;
+    gender: Gender;
+    age: Number;
+    pets: Array<Pet>;
 }
